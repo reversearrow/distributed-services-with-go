cdfb8900dd335a4f16ddc676421826128be0e270
jayshah <reversearrow@gmail.com>
2023-09-06 09:45:09 -0400

    add config file

--- log/config.go
+++ log/config.go
@@ -2,9 +2,12 @@ package log
 
 // Config to centralize configuration for the log package
 type Config struct {
-	Segment struct {
-		MaxStoreBytes uint64
-		MaxIndexBytes uint64
-		InitialOffset uint64
-	}
+	Segment Segment
+}
+
+// Segment stores configuration for the segment.
+type Segment struct {
+	MaxStoreBytes uint64
+	MaxIndexBytes uint64
+	InitialOffset uint64
 }
